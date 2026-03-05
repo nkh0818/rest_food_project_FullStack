2e4e0f932e3e184ada237450a475853056baccc3
nkh0818 <sunder10210@gmail.com>
2026-03-05 11:08:50 +0900

    오류잡음..

--- src/main/java/com/yonsai/rest_food_project/RestFoodProjectApplication.java
+++ src/main/java/com/yonsai/rest_food_project/RestFoodProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestFoodProjectApplication {
 
 	public static void main(String[] args) {
+		io.github.cdimascio.dotenv.Dotenv.configure().systemProperties().load();
 		SpringApplication.run(RestFoodProjectApplication.class, args);
 	}
 
--- src/main/java/com/yonsai/rest_food_project/repository/UserRepository.java
+++ src/main/java/com/yonsai/rest_food_project/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.yonsai.rest_food_project.repository;
 
+import com.yonsai.rest_food_project.entity.User; // User 엔티티 위치
+import org.springframework.data.jpa.repository.JpaRepository; // JPA 인터페이스
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
